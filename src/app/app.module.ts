9182be99941b7642cf98f336b996aff89f2afdbf
harish tadikonda <harish11.tadikonda@gmail.com>
2018-07-11 00:26:33 -0400

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CentralPanelComponent } from './central-panel/central-panel.component';
+import {RestProviderService} from './rest-provider.service';
+import { FilterPipe} from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    CentralPanelComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [RestProviderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit , Input } from '@angular/core';
+import { RestProviderService } from '../rest-provider.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  @Input() searchText: string;
+  public peopleList;
+
+  constructor(public peopleService:RestProviderService) { 
+    this.peopleList=peopleService.peopleList;
+  }
+
+  selectPerson(person){
+      this.peopleService.selectedPerson=person;
+  }
+
+  ngOnInit() {
+  }
+
+}
